599c06e606a69194f1032bfffce141fc8fdb7be4
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-06 15:16:23 -0300

    iniciando o projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  
+
 }
 
 module.exports = getSpeciesByIds;
-const vamosComTudo = ['BoraTryber', 'VamosComTudo']
-console.log(vamosComTudo)
+const vamosComTudo = ['BoraTryber', 'VamosComTudo'];
+console.log(vamosComTudo);
