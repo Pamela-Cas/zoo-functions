c6698a984b87836c3c999e91cd6a699fec183cbd
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-06 16:16:14 -0300

    Requesito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalsArray = data.species.find((specie) => specie.name === animal);
+  // Primeiro procura o nome do animal dentro de 'species'.
+  return animalsArray.residents.every((ages) => ages.age >= age);
+  // Em seguida busca todos os animais com que tenham a idade maior ou igual à passada no parâmetro.
 }
 
 module.exports = getAnimalsOlderThan;
+// referencia https://warcontent.com/array-find-javascript/
+// https://ricardo-reis.medium.com/every-do-array-javascript-e0cf24607164
