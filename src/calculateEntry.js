79a1b3065b8449b8abe9fe429fdbad0218b19e07
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-10 21:57:38 -0300

    requesito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  // eslint-disable-next-line editorconfig/editorconfig
+  const child = entrants.filter((entrada) => entrada.age < 18).length;
+  const adult = entrants.filter((entrada) => entrada.age >= 18 && entrada.age < 50).length;
+  const senior = entrants.filter((entrada) => entrada.age >= 50).length;
+  const quantd = {
+    child,
+    adult,
+    senior,
+  };
+  return quantd;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !Object.keys(entrants).length) return 0;
+  const obj = countEntrants(entrants);
+  const qtdAdult = Object.entries(obj).find((valor) => valor[0] === 'adult')[1];
+  const qtdChild = Object.entries(obj).find((valor) => valor[0] === 'child')[1];
+  const qtdSenior = Object.entries(obj).find((valor) => valor[0] === 'senior')[1];
+  const pricesAdult = Object.entries(data.prices).find((valor) => valor[0] === 'adult')[1];
+  const pricesChild = Object.entries(data.prices).find((valor) => valor[0] === 'child')[1];
+  const pricesSenior = Object.entries(data.prices).find((valor) => valor[0] === 'senior')[1];
+  const valorTotal = pricesAdult * qtdAdult + pricesChild * qtdChild + pricesSenior * qtdSenior;
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
