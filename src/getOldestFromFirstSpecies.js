899d3139156097b0043ce34fa36bc38897fb035a
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-17 10:29:46 -0300

    alterando funcao getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,8 @@
-
 const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const zookeeper = employees.find((employee) => employee.id === id).responsibleFor[0];
-  return Object.values(species.reduce((spec) => spec.id === zookeeper)
+  return Object.values(species.find((spec) => spec.id === zookeeper)
     .residents.sort((a, b) => b.age - a.age)[0]);
 }
 module.exports = getOldestFromFirstSpecies;
