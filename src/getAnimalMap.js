e0447661e89d5561f5eea059ad5d0aa178abdda6
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-17 02:21:19 -0300

    testando funcao getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,13 +1,46 @@
+
+  
+const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
-function getAnimalMap(option) {
-  if (!option) {
-    return species.reduce((acc, spec) => (
-      { ...acc, [spec.name]: spec.residents.location.length }
-    ), {});
-  }
+const getAnimalsForLocation = () => {
+  const animalsLocation = {
+    NE: species
+      .filter((specie) => specie.location === 'NE')
+      .map((animal) => animal.name),
+    NW: species
+      .filter((specie) => specie.location === 'NW')
+      .map((animal) => animal.name),
+    SE: species
+      .filter((specie) => specie.location === 'SE')
+      .map((animal) => animal.name),
+    SW: species
+      .filter((specie) => specie.location === 'SW')
+      .map((animal) => animal.name),
+  };
+  return animalsLocation;
+};
+
+function getAnimalsIncludeOptions(sorted, sex) {
+  const emptyObject = {};
+  species.forEach((specie) => {
+    if (!emptyObject[specie.location]) emptyObject[specie.location] = [];
+    let residents = [...specie.residents];
+    if (sex) residents = residents.filter((specieSex) => specieSex.sex === sex);
+    residents = residents.map((resident) => resident.name);
+    if (sorted) residents.sort();
+    emptyObject[specie.location].push({ [specie.name]: residents });
+  });
+  return emptyObject;
 }
 
-console.log(getAnimalMap());
+function getAnimalMap(options) {
+  if (options === undefined) {
+    return getAnimalsForLocation();
+  }
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) return getAnimalsIncludeOptions(sorted, sex);
+  return getAnimalMap();
+}
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,19 @@
 const data = require('../data/zoo_data');
 
-const { hours, species } = data;
+function getSchedule(scheduleTarget) {
+  // seu código aqui
+}
+// const { hours, species } = data;
 
-/* Sem parâmetros: horários para cada dia e quais animais disponíveis; */
+// // Sem parâmetros: horários para cada dia e quais animais disponíveis; */
 // const semParam = () => {
-//   //const hora = data.hours;
-//   const animal = data.species.filter((name) => (species.name))
-//   return animal;
+// const hora = data.hours;
+// const animal = data.species.filter((name) => (species.name))
+// return animal;
 // };
 // console.log(semParam());
 
-/* const animalName = data.species.filter((name) => (name
-  .firstName === employeeName || name.lastName === employeeName));
-return employee; */
+// const animalName = data.species.filter((name) => (name
+//   .firstName === employeeName || name.lastName === employeeName));
+// return employee;
+module.exports = getSchedule;
