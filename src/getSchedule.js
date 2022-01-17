aa67294340f9bec44a0e2f2d095eae2b0d100f83
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-17 09:24:56 -0300

    arrumando erros lint item 7

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,8 +1,3 @@
-const data = require('../data/zoo_data');
-
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
 // const { hours, species } = data;
 
 // // Sem parâmetros: horários para cada dia e quais animais disponíveis; */
@@ -16,4 +11,3 @@ function getSchedule(scheduleTarget) {
 // const animalName = data.species.filter((name) => (name
 //   .firstName === employeeName || name.lastName === employeeName));
 // return employee;
-module.exports = getSchedule;
